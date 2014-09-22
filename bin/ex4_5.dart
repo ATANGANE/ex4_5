42a29e769e2fe03f2eeacec182de7d3d98e868c5
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-09-22 16:47:19 +0200

    add dr.txt

--- bin/ex4_5.dart
+++ bin/ex4_5.dart
@@ -1,4 +1,14 @@
+/*
 Int mult(int n, int i)
 {return n*i;}
 Void main()
 {for(i=0;i<16;i++) print(mult(8,i));}
+ * */
+
+int mult(int n, int i) {
+  return n * i;
+}
+
+main() {
+  for(var i = 0; i <= 16; i++) print(mult(8,i));
+}
